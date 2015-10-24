5472535f7baacd9ae493dcd718cac0f7772cc1cb
4amic <4amicosta@gmail.com>
2015-10-23 17:49:25 -0700

    Adding named route for show_by_name route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+    get "pins/name-:slug" => "pins#show_by_name", as: 'pin_by_name'
+    
   # You can have the root of your site routed with "root"
   root 'pins#index'
 
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
     get '/library' => 'pins#index'
+    
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
